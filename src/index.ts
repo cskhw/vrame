32706fdf3c56b8b6e0f8726d471ca17b5c2f58de
cskhw <51100329+cskhw@users.noreply.github.com>
2022-12-05 11:27:46 +0900

    feat: test

--- src/index.ts
+++ src/index.ts
@@ -53,8 +53,7 @@ export function createVrame(vrameConfig?: VrameConfig): Plugin {
       // vrameStyle.setAttribute("type", "text/css");
       // vrameStyle.innerHTML = style;
       // document.head.appendChild(vrameStyle);
-      // test
-      // test
+      // testmain
     },
   };
 }
